f138d2cfce1f00293db3dea5ea330ec49e14673b
Will Strongman <WStrongman@stu.kerikerihigh.ac.nz>
2021-07-01 10:54:52 +1200

    amking things work better

--- quiz.py
+++ quiz.py
@@ -1,13 +1,20 @@
 import requests
+from html import unescape
 
 response = requests.get("https://opentdb.com/api.php?amount=10&category=31&difficulty=easy&type=boolean")
+# html.unescape(response)
+questions =response.json()["results"]
 
-questions = response.json()["results"]
+print('\033[95m'+"""------------------welcome------------------
+this is a quiz about anime,
+there are 10 questions that will need to be answered by you
+these are going to be either True or False
+make sure ur spelling is correct otherwise you`ll get the question wrong """)
 
 for i, question in enumerate(questions) or i > 11:
 
     print(f"---------question {i + 1}---------")
-    ans = input(question["question"])
+    ans = input(unescape(question["question"]))
 
     if ans == question["correct_answer"]:
         print("correct!!")
