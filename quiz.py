868ceb4fd57ada94575466c740e43f07076a0028
Will Strongman <WStrongman@stu.kerikerihigh.ac.nz>
2021-08-09 13:03:59 +1200

    finnally i got spelling to work as well as proper output (:

--- quiz.py
+++ quiz.py
@@ -3,7 +3,8 @@ from html import unescape
 
 response = requests.get("https://opentdb.com/api.php?amount=10&category=31&difficulty=easy&type=boolean")
 # html.unescape(response)
-questions =response.json()["results"]
+questions = response.json()["results"]
+print(questions[0])
 
 print('\033[95m'"--------------------welcome--------------------")
 print('\033[34m'+"""
@@ -18,18 +19,20 @@ for i, question in enumerate(questions) or i > 11:
 
 #this tells the question and then gives feedback on there input
     print('\033[95m'f"-------------------question {i + 1}-------------------")
-    ans = input(unescape(question["question"])) #this converts some invalid charectors and then prints the full question
-   
-    if ans == question["correct_answer"]:
-        print('\033[32m ' "correct!!")
+    ans = input(unescape(question["question"]))                          #this converts some invalid charectors and then prints the full question
+
+
+    ans_cap = ans.capitalize()
+
+    if ans_cap in question["incorrect_answers"]:
+        print("wrong")
+
+    if ans_cap == question["correct_answer"]:
+        print('\033[32m' "correct!!")
         score = score + 1
 
-    if ans == question["incorrect_answers"]:
-        print('\033[91m'"wrong")
-
-
-#    if ans not in ("True", "False"):
-#        print("invalid response please use True or False")
+    if ans_cap not in ("True", "False"):
+        print("invalid response please use True or False")
 
 
 
@@ -40,5 +43,5 @@ print(f"your total score out of {i} questions is {score}")
 print("thank you for using this quiz") 
 
 
-
+#'\033[91m'
 
